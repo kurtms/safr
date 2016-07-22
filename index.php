645e9c2cf241795df141b0c9ffc1d97d815a59c8
Kurt Schneider <kurtms@live.com>
2016-07-21 21:58:25 -0700

    added

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <title>SaferSpace</title>
+    <link rel="icon" type="img/ico" href="img/favicon.ico">
     <link rel="stylesheet" href="main.css" type="text/css">
 </head>
 <body>
@@ -19,6 +20,16 @@
     <div class="dark-primary-color">
         <h1>SaferSpace</h1>
     </div>
+    <div class="divider divider-color"></div>
+    <div class="default-primary-color">
+        <p>A safer space for everyone</p>
+    </div>
+</div>
+<div id="header header-placeholder" class="text-primary-color">
+    <div class="dark-primary-color">
+        <h1>SaferSpace</h1>
+    </div>
+    <div class="divider divider-color"></div>
     <div class="default-primary-color">
         <p>A safer space for everyone</p>
     </div>
